318930dc468c350ab1a7734bd25708dcddb8a710
Rahul Kashyap <satyamtiwari736182@gmail.com>
2024-01-08 20:36:43 +0530

    readme added

--- chat-app/src/message.constant.ts
+++ chat-app/src/message.constant.ts
@@ -1,35 +1,35 @@
 const map: Map<string, string> = new Map();
-map.set('hi', 'Hi, how can I help you');
-map.set('sure', 'Reply: Great! Let us do it.');
-map.set('hello', 'Hello, how can I help you');
-map.set('you', 'I am good, thanks. How about you?');
+map.set("hi", "Hi, how can I help you");
+map.set("sure", "Reply: Great! Let us do it.");
+map.set("hello", "Hello, how can I help you");
+map.set("you", "I am good, thanks. How about you?");
 map.set(
-  'help',
-  'Of course! I am here to help. What do you need assistance with?',
+  "help",
+  "Of course! I am here to help. What do you need assistance with?",
 );
 map.set(
-  'ai',
-  'ertainly! How can I assist you with AI? Are you looking for information, have questions about a specific aspect of AI, or need help with a project related to artificial intelligence? Feel free to provide more details so I can better assist you',
+  "ai",
+  "ertainly! How can I assist you with AI? Are you looking for information, have questions about a specific aspect of AI, or need help with a project related to artificial intelligence? Feel free to provide more details so I can better assist you",
 );
 map.set(
-  'good',
-  'I am glad to hear that! If you have any more questions or if there is anything else I can help you with, feel free to ask.',
+  "good",
+  "I am glad to hear that! If you have any more questions or if there is anything else I can help you with, feel free to ask.",
 );
 map.set(
-  'ok',
-  'I am glad to hear that! If you have any more questions or if there is anything else I can help you with, feel free to ask.',
+  "ok",
+  "I am glad to hear that! If you have any more questions or if there is anything else I can help you with, feel free to ask.",
 );
 map.set(
-  'bye',
-  'Goodbye! If you ever have more questions or need assistance in the future, do not hesitate to reach out. Have a great day',
+  "bye",
+  "Goodbye! If you ever have more questions or need assistance in the future, do not hesitate to reach out. Have a great day",
 );
 map.set(
-  'thank',
-  'You are welcome! If you ever need help again, feel free to return. Take care and have a wonderful day!',
+  "thank",
+  "You are welcome! If you ever need help again, feel free to return. Take care and have a wonderful day!",
 );
 map.set(
-  'thanks',
-  'You are welcome! If you ever need help again, feel free to return. Take care and have a wonderful day!',
+  "thanks",
+  "You are welcome! If you ever need help again, feel free to return. Take care and have a wonderful day!",
 );
 
 export default map;
